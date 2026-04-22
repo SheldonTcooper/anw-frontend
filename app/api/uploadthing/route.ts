14574d27274b7266a15a517a97f7e924f70a2191
Romulo Carracho <romulo@tauritecnologia.com.br>
2026-04-21 22:35:20 -0300

    fix: corrige tipo do uploadthing route

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -1,14 +1,15 @@
 import { createRouteHandler, createUploadthing } from "uploadthing/next";
+import type { FileRouter } from "uploadthing/next";
 
 const f = createUploadthing();
 
-export const ourFileRouter = {
+const ourFileRouter = {
   imageUploader: f({
     image: { maxFileSize: "4MB", maxFileCount: 10 },
   }).onUploadComplete(async ({ file }) => {
     return { url: file.url };
   }),
-};
+} satisfies FileRouter;
 
 export type OurFileRouter = typeof ourFileRouter;
 
