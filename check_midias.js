21d6ee500f0d364a97b3945ec3f49b45e391a251
Romulo Carracho <romulo@tauritecnologia.com.br>
2026-04-23 19:17:15 -0300

    feat: rate limiting em login (5/min) e registro (3/hora)

---
+++ check_midias.js
@@ -0,0 +1,9 @@
+﻿const { PrismaClient } = require('./node_modules/.prisma/client')
+const prisma = new PrismaClient()
+async function main() {
+  const midias = await prisma.midias.findMany({
+    select: { id: true, url: true, isCapa: true, aprovada: true, anuncioId: true }
+  })
+  console.log(JSON.stringify(midias, null, 2))
+}
+main().then(() => prisma.$disconnect()).catch(console.error)
---
+++ fix_midias.js
@@ -0,0 +1,30 @@
+﻿const { PrismaClient } = require('./node_modules/.prisma/client')
+const prisma = new PrismaClient()
+async function main() {
+  // Aprova todas as midias
+  const aprovadas = await prisma.midias.updateMany({
+    data: { aprovada: true }
+  })
+  console.log('Midias aprovadas:', aprovadas.count)
+
+  // Define a primeira midia de cada anuncio como capa
+  const midias = await prisma.midias.findMany({
+    orderBy: { ordem: 'asc' }
+  })
+  
+  const porAnuncio = {}
+  for (const m of midias) {
+    if (!porAnuncio[m.anuncioId]) {
+      porAnuncio[m.anuncioId] = m.id
+    }
+  }
+
+  for (const [anuncioId, midiaId] of Object.entries(porAnuncio)) {
+    await prisma.midias.update({
+      where: { id: midiaId },
+      data: { isCapa: true }
+    })
+  }
+  console.log('Capas definidas:', Object.keys(porAnuncio).length)
+}
+main().then(() => prisma.$disconnect()).catch(console.error)
